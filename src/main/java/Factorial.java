affbab4db22fa4c0f2027a1416dafb20ecca9f87
Matthias Hiemer <matthias.hiemer@neuefische.de>
2022-03-24 15:00:47 +0100

    Add factorial method and tests

---
+++ src/main/java/Factorial.java
@@ -0,0 +1,16 @@
+public class Factorial {
+
+    public static void main(String[] args) {
+        int fact = factorial(10);
+        System.out.println(fact);
+    }
+
+    public static int factorial(int number) {
+
+        if (number == 0){
+            return 1;
+        }
+
+        return number * factorial(number -1);
+    }
+}
---
+++ src/test/java/FactorialTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FactorialTest {
+
+    @Test
+    void testfactorial_0() {
+        assertEquals(1, Factorial.factorial(0));
+    }
+
+    @Test
+    void testfactorial_1() {
+        assertEquals(1, Factorial.factorial(1));
+    }
+
+    @Test
+    void testfactorial_2() {
+        assertEquals(2, Factorial.factorial(2));
+    }
+
+    @Test
+    void testfactorial_3() {
+        assertEquals(6, Factorial.factorial(3));
+    }
+
+    @Test
+    void testfactorial_10() {
+        assertEquals(3628800, Factorial.factorial(10));
+    }
+
+    @Test
+    void testfactorial_negativeNumber() {
+        assertEquals(1, Factorial.factorial(-1));
+    }
+
+}
