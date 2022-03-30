b5125a808b46b924c2dc25ebce661b14275f3243
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-03-30 16:54:49 +0200

    Add arryList

--- src/main/java/exercises/StudentDB.java
+++ src/main/java/exercises/StudentDB.java
@@ -2,68 +2,45 @@ package exercises;
 
 import model.Student;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class StudentDB {
 
-    private Student[] students;
+    private List<Student> students;
 
-    public StudentDB(Student[] insertedStudents) {
+    public StudentDB(List insertedStudents) {
         this.students = insertedStudents;
     }
 
-     public Student[] getAllStudents() {
+     public List<Student> getAllStudents() {
         return students;
     }
-
-    @Override
-    public String toString() {
-
-        return "exercises.StudentDB{" +
-                "students=" + Arrays.toString(students) +
-                '}';
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     public Student getRandomStudent() {
 
         double randomDouble = Math.random();
-        return students[(int) (randomDouble * students.length)];
+        return students.get((int) (randomDouble * students.size()));
     }
 
     public void add(Student newStudent) {
-        Student[] newStudents = Arrays.copyOf(students, students.length + 1);
-        newStudents[newStudents.length - 1] = newStudent;
-        this.students = newStudents;
+        students.add(newStudent);
     }
 
     public void remove(int id) {
-
-        for (int i = 0; i < students.length; i++) {
-            if (students[i].getId() == id) {
-                // Remove and create new array
-                Student[] newStudents = Arrays.copyOf(students, students.length - 1);
-                System.arraycopy(students, 0, newStudents, 0, i);
-                System.arraycopy(students, i + 1, newStudents, i, students.length - (i + 1));
-                this.students = newStudents;
+        for (Student currentStudent : students) {
+            if (currentStudent.getId() == id) {
+                students.remove(currentStudent);
+                return;
             }
         }
     }
 
+    @Override
+    public String toString() {
+        return "StudentDB{" +
+                "students=" + students +
+                '}';
+    }
 }
--- src/test/java/exercises/StudentDBTest.java
+++ src/test/java/exercises/StudentDBTest.java
@@ -4,28 +4,33 @@ import model.Student;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentDBTest {
 
     @Test
     void shouldMakeSureThatStudentsCanBeRetrieved() {
         // given
-        Student[] students = { new Student(1, "André Schreck"), new Student(2, "Thomas Kittlaus") };
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "André Schreck"));
+        students.add(new Student(2, "Thomas Kittlaus"));
         StudentDB studentDB = new StudentDB(students);
 
         // when
-        Student[] actual = studentDB.getAllStudents();
+        List<Student> actual = studentDB.getAllStudents();
 
         // then
-        Assertions.assertArrayEquals(students, actual);
+        Assertions.assertEquals(students, actual);
 
-        Assertions.assertEquals(1, actual[0].getId());
-        Assertions.assertEquals("André Schreck", actual[0].getName());
+        Assertions.assertEquals(1, actual.get(0).getId());
+        Assertions.assertEquals("André Schreck", actual.get(0).getName());
     }
 
     @Test
     void shouldReturnRandomStudent() {
         // given
-        Student[] students = { new Student(1, "André Schreck"), new Student(2, "Thomas Kittlaus") };
+        List<Student> students = new ArrayList<>(List.of(new Student(1, "André Schreck"), new Student(2, "Thomas Kittlaus")));
         StudentDB studentDB = new StudentDB(students);
 
         // when
@@ -38,73 +43,73 @@ public class StudentDBTest {
     @Test
     void shouldReturnStringRepresentation() {
         // given
-        Student[] students = { new Student(1, "André Schreck"), new Student(2, "Thomas Kittlaus") };
+        List<Student> students = new ArrayList<>(List.of(new Student(1, "André Schreck"), new Student(2, "Thomas Kittlaus")));
         StudentDB studentDB = new StudentDB(students);
 
         // when
         String actual = studentDB.toString();
 
         // then
-        Assertions.assertEquals("exercises.StudentDB{students=[Student{id=1, name='André Schreck', birthday=null}, Student{id=2, name='Thomas Kittlaus', birthday=null}]}", actual);
+        Assertions.assertEquals("StudentDB{students=[Student{id=1, name='André Schreck', birthday=null}, Student{id=2, name='Thomas Kittlaus', birthday=null}]}", actual);
     }
 
     @Test
     void shouldAddNewStudent() {
         // given
-        Student[] students = { new Student(1, "André Schreck") };
+        List<Student> students = new ArrayList<>(List.of(new Student(1, "André Schreck")));
         StudentDB studentDB = new StudentDB(students);
 
         // when
         studentDB.add(new Student(2, "Thomas Kittlaus"));
-        Student[] actual = studentDB.getAllStudents();
+        List<Student> actual = studentDB.getAllStudents();
 
         // then
-        Student[] expected = { new Student(1, "André Schreck"), new Student(2, "Thomas Kittlaus") };
-        Assertions.assertArrayEquals(expected, actual);
+        List<Student> expected = List.of(new Student(1, "André Schreck"), new Student(2, "Thomas Kittlaus"));
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void shouldRemoveFirstStudent() {
         // given
-        Student[] students = { new Student(1, "André Schreck"), new Student(2, "Thomas Kittlaus") };
+        List<Student> students = new ArrayList<>(List.of(new Student(1, "André Schreck"), new Student(2, "Thomas Kittlaus")));
         StudentDB studentDB = new StudentDB(students);
 
         // when
         studentDB.remove(1);
-        Student[] actual = studentDB.getAllStudents();
+        List<Student> actual = studentDB.getAllStudents();
 
         // then
-        Student[] expected = { new Student(2, "Thomas Kittlaus") };
-        Assertions.assertArrayEquals(expected, actual);
+        List<Student> expected = List.of(new Student(2, "Thomas Kittlaus"));
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void shouldRemoveLastStudent() {
         // given
-        Student[] students = { new Student(1, "André Schreck"), new Student(2, "Thomas Kittlaus") };
+        List<Student> students = new ArrayList<>(List.of(new Student(1, "André Schreck"), new Student(2, "Thomas Kittlaus")));
         StudentDB studentDB = new StudentDB(students);
 
         // when
         studentDB.remove(2);
-        Student[] actual = studentDB.getAllStudents();
+        List<Student> actual = studentDB.getAllStudents();
 
         // then
-        Student[] expected = { new Student(1, "André Schreck") };
-        Assertions.assertArrayEquals(expected, actual);
+        List<Student> expected = List.of(new Student(1, "André Schreck"));
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void shouldRemoveIntermediateStudent() {
         // given
-        Student[] students = { new Student(1, "André Schreck"), new Student(2, "Max Mustermann"), new Student(3, "Thomas Kittlaus") };
+        List<Student> students = new ArrayList<>(List.of(new Student(1, "André Schreck"), new Student(2, "Max Mustermann"), new Student(3, "Thomas Kittlaus")));
         StudentDB studentDB = new StudentDB(students);
 
         // when
         studentDB.remove(2);
-        Student[] actual = studentDB.getAllStudents();
+        List<Student> actual = studentDB.getAllStudents();
 
         // then
-        Student[] expected = { new Student(1, "André Schreck"), new Student(3, "Thomas Kittlaus") };
-        Assertions.assertArrayEquals(expected, actual);
+        List<Student> expected = List.of(new Student(1, "André Schreck"), new Student(3, "Thomas Kittlaus"));
+        Assertions.assertEquals(expected, actual);
     }
 }
